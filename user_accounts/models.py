26920218bf228790e07af5b4bcf9515c33ed0394
jose-gonzalez523 <jose.gonzalez@drake.edu>
2021-05-06 18:42:56 -0500

    Updated Models.py

--- user_accounts/models.py
+++ user_accounts/models.py
@@ -1,15 +1,22 @@
 from flask_login import UserMixin
 from . import db
+import datetime
 
 class User(UserMixin,db.Model):
+    __tablename__='user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    events = db.relationship('Events')
 
 class Events(UserMixin, db.Model):
+    __tablename__='events'
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship("User", backref=db.backref('user',uselist='false'))
+
     name = db.Column(db.String(1000))
     event_name = db.Column(db.String(1000))
-    date_time = db.Column(db.datetime(timezone=True))
+    date_time = db.Column(db.DateTime(timezone=True))
     caption = db.Column(db.String(1000))
